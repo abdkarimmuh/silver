40f551fbba0bea9cf7720d176ae402df583cc1d1
Muhammad Abdul Karim <alkariim.tridek@gmail.com>
2021-10-30 22:24:02 +0700

    bismillah

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'silver',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: '/vendors/@mdi/font/css/materialdesignicons.min.css',
+      },
+      { rel: 'stylesheet', href: '/css/flat-ui-kit.css' },
+      { rel: 'stylesheet', href: '/css/miri-ui-kit-free.css' },
+      { rel: 'stylesheet', href: '/css/demo.css' },
+    ],
+    script: [
+      { src: '/vendors/jquery/dist/jquery.min.js' },
+      { src: '/vendors/popper.js/dist/umd/popper.min.js' },
+      { src: '/vendors/bootstrap/dist/js/bootstrap.min.js' },
+      { src: '/js/miri-ui-kit.js' },
+    ],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['~/assets/css/demo.css.map'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en',
+    },
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+}
