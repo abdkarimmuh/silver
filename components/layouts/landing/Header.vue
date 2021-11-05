7074228b8541891f5a24b8fe2eb8fd8dce993218
Muhammad Abdul Karim <alkariim.tridek@gmail.com>
2021-11-06 01:08:45 +0700

    Update Header

--- components/layouts/landing/Header.vue
+++ components/layouts/landing/Header.vue
@@ -28,32 +28,6 @@
               <a class="nav-link" href="#home">Beranda</a>
             </li>
 
-            <!-- <li class="nav-item dropdown">
-              <a
-                href="#about"
-                class="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-              >
-                Tentang
-              </a>
-              <div class="dropdown-menu dropdown-menu-right">
-                <a href="landing.html" class="dropdown-item"
-                  ><i class="dropdown-item-icon mdi mdi-shape-outline"></i
-                  >Landing Page</a
-                >
-                <a href="login.html" class="dropdown-item"
-                  ><i class="dropdown-item-icon mdi mdi-lock-outline"></i>Login
-                  Page</a
-                >
-                <a href="profile.html" class="dropdown-item"
-                  ><i
-                    class="dropdown-item-icon mdi mdi-shield-account-outline"
-                  ></i
-                  >Profile Page</a
-                >
-              </div>
-            </li> -->
-
             <li class="nav-item">
               <a class="nav-link" href="#about">Tentang</a>
             </li>
@@ -66,20 +40,6 @@
               <a class="nav-link" href="#blog">Blog</a>
             </li>
 
-            <li class="nav-item mx-3">
-              <a class="nav-link nav-icon icon-fb" href="#"
-                ><i class="mdi mdi-facebook-box"></i
-              ></a>
-
-              <a class="nav-link nav-icon icon-twitter" href="#"
-                ><i class="mdi mdi-twitter-box"></i
-              ></a>
-
-              <a class="nav-link nav-icon icon-insta" href="#"
-                ><i class="mdi mdi-instagram"></i
-              ></a>
-            </li>
-
             <form action="#" class="form-inline ml-lg-3">
               <button class="btn btn-outline-primary btn-sm">Gabung</button>
             </form>
@@ -87,30 +47,6 @@
         </div>
       </div>
     </nav>
-    <!-- <div
-      class="
-        miri-ui-kit-header-content
-        text-center text-white
-        d-flex
-        flex-column
-        justify-content-center
-        align-items-center
-        px-5
-      "
-    >
-      <h1 class="display-3 text-white">Silver</h1>
-      <p class="h3 font-weight-light text-white">
-        A beautiful premium bootstrap 4 developed by bootstrapdash.
-      </p>
-      <p class="mt-4">
-        <button
-          class="btn btn-soft-primary btn-pill mr-2"
-          @click="$router.push('/#about')"
-        >
-          Selengkapnya
-        </button>
-      </p>
-    </div> -->
     <div
       class="
         miri-ui-kit-header-content
@@ -123,23 +59,24 @@
       <p class="h3 font-weight-light text-white">
         A beautiful premium bootstrap 4 developed by bootstrapdash.
       </p>
-      <p class="mt-3">
+      <p class="mt-3 mb-5">
         <button class="btn btn-soft-primary" @click="$router.push('/#about')">
           Selengkapnya
         </button>
       </p>
-      <div class="row">
-        <a class="nav-link nav-icon icon-fb" href="#"
-          ><i class="mdi mdi-facebook-box"></i
-        ></a>
-
-        <a class="nav-link nav-icon icon-twitter" href="#"
-          ><i class="mdi mdi-twitter-box"></i
-        ></a>
-
-        <a class="nav-link nav-icon icon-insta" href="#"
-          ><i class="mdi mdi-instagram"></i
-        ></a>
+    </div>
+    <div class="container">
+      <div class="header-social pb-5">
+        <h6 class="text-white">Ikuti Sosial Media Kami</h6>
+        <a class="header-social-icon icon-fb pr-2" href="#">
+          <i class="mdi mdi-facebook-box"></i>
+        </a>
+        <a class="header-social-icon icon-twitter pr-2" href="#">
+          <i class="mdi mdi-twitter-box"></i>
+        </a>
+        <a class="header-social-icon icon-insta pr-2" href="#">
+          <i class="mdi mdi-instagram"></i>
+        </a>
       </div>
     </div>
   </header>
