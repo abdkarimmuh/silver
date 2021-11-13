10aba0af25984e4ae1445f9d1a047dbf4d0e6eee
Muhammad Abdul Karim <alkariim.tridek@gmail.com>
2021-11-13 14:35:08 +0700

    vue-youtube

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,7 @@ export default {
   css: ['~/assets/css/custom.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '~/plugins/persistedState.client.js' }],
+  plugins: ['~/plugins/persistedState.client.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
