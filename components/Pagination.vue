63de840e7eb788ebbcb0880fd90c3c38b887bb1f
Muhammad Abdul Karim <alkariim.tridek@gmail.com>
2021-11-21 00:48:00 +0700

    logic paginate

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -2,23 +2,27 @@
   <nav>
     <ul class="pagination pagination-pill">
       <li :class="'page-item' + (disabledPrev ? ' disabled' : '')">
-        <a class="page-link" href="" @click.prevent="prev()"
-          ><i class="mdi mdi-chevron-left"></i
-        ></a>
+        <a class="page-link" href="" @click.prevent="prev()">
+          <i class="mdi mdi-chevron-left"></i>
+        </a>
       </li>
       <li
-        v-for="item in paginationList"
-        :key="item"
-        :class="'page-item' + (item === activePage ? ' active' : '')"
+        v-for="(item, index) in paginationList"
+        :key="index"
+        :class="
+          'page-item' +
+          (item === activePage ? ' active' : '') +
+          (item === '...' ? ' disabled' : '')
+        "
       >
         <a class="page-link" href="" @click.prevent="changePage(item)">
           {{ item }}
         </a>
       </li>
       <li :class="'page-item' + (disabledNext ? ' disabled' : '')">
-        <a class="page-link" href="" @click.prevent="next()"
-          ><i class="mdi mdi-chevron-right"></i
-        ></a>
+        <a class="page-link" href="" @click.prevent="next()">
+          <i class="mdi mdi-chevron-right"></i>
+        </a>
       </li>
     </ul>
   </nav>
@@ -35,13 +39,66 @@ export default {
   },
   computed: {
     paginationList() {
-      const arr = Array.from({ length: this.maxPage }, (_, i) => i + 1)
-      return arr
+      let arr = Array.from({ length: this.maxPage }, (_, i) => i + 1)
+      if (arr.length > 5) {
+        if (this.activePage === 1 || this.activePage === this.maxPage) {
+          arr = arr.map((element) => {
+            if (Math.abs(element - this.activePage) <= 5) {
+              return element
+            }
+            return '...'
+          })
+        } else if (
+          this.activePage === 2 ||
+          this.activePage === this.maxPage - 1
+        ) {
+          arr = arr.map((element) => {
+            if (Math.abs(element - this.activePage) <= 4) {
+              return element
+            }
+            return '...'
+          })
+        } else if (
+          this.activePage === 3 ||
+          this.activePage === this.maxPage - 2
+        ) {
+          arr = arr.map((element) => {
+            if (Math.abs(element - this.activePage) <= 3) {
+              return element
+            }
+            return '...'
+          })
+        } else {
+          arr = arr.map((element) => {
+            if (Math.abs(element - this.activePage) <= 2) {
+              return element
+            }
+            return '...'
+          })
+        }
+      }
+      let paginate = []
+      arr.forEach((element, index) => {
+        if (index === 0) {
+          paginate = [element]
+          return
+        }
+        if (index === arr.length - 1) {
+          paginate.push(element)
+          return
+        }
+        if (element !== '...') {
+          paginate.push(element)
+        }
+      })
+      return paginate
     },
   },
   methods: {
     changePage(page) {
-      this.$emit('changePaginate', page)
+      if (page !== '...') {
+        this.$emit('changePaginate', page)
+      }
     },
     prev() {
       if (!this.disabledPrev) {
